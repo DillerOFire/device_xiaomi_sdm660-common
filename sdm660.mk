0ea449784642f05a441222b99c3f200dcfad09da
VerpaPhullus <niktofe1@gmail.com>
2020-10-17 13:05:52 +1000

    Try to fix bt

--- sdm660.mk
+++ sdm660.mk
@@ -476,6 +476,16 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    BluetoothQti \
+    libbthost_if \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
